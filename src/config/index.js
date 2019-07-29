5ee4e1a1c558efd2be217fe09d9bed6380083f10
valverde82 <valverde.marcelo@gmail.com>
2019-07-29 17:14:47 -0300

    refactor server... add uuid generator for movieid

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 export const server = {
-    host: '192.168.15.2',
+    host: 'localhost',
     ws_port: 9090,
     web_port: 8080
 };
 
 export const endpoint = `ws://${server.host}:${server.ws_port}`;
 
-export const is_static = false;
+export const is_static = true;
